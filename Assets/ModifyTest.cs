8816f8f813ee525294bc47c58ac7062362843663
Aminos <147380126+2497812382@users.noreply.github.com>
2026-06-16 17:02:47 +0800

    no message

--- Assets/ModifyTest.cs
+++ Assets/ModifyTest.cs
@@ -7,8 +7,8 @@ public class ModifyTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        int doSth = 0;
-        int doSth0 = 0;
+        int doSth = 1;
+        int doSth0 = 2;
     }
 
     // Update is called once per frame
