67e8f46ec3632f03e5170564d622b1179f60dbc7
Aminos <147380126+2497812382@users.noreply.github.com>
2026-06-23 11:54:03 +0800

    no message

--- Assets/Script/MainAsset.cs
+++ Assets/Script/MainAsset.cs
@@ -7,10 +7,6 @@ public class MainAsset : ScriptableObject
     // Start is called before the first frame update
     void Start()
     {
-        
-        
-        
-        
     }
 
     // Update is called once per frame
